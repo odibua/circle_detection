dc0ff8047354371444732e5e5402cedcfa559742
odibua <odibua@gmail.com>
2020-02-20 09:51:27 -0800

    Change CNN backbone

--- train.py
+++ train.py
@@ -5,9 +5,9 @@ import visdom
 import torch
 import torch.nn as nn
 from collections import OrderedDict
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 import torch.optim as optim
-from utils import *
+from utils import iou, noisy_circle
 from shapely.geometry import Point, GeometryCollection
 
 
@@ -24,17 +24,33 @@ def giou(params0, params1):
             collection.area
     )
 
+class GIOULOSS(nn.Module):
+
+    def __init__(self):
+        super(GIOULOSS, self).__init()
+
+    def forward(self, x, y):
+        x_npy, y_npy = x.data.numpy(), y.data.numpy()
+        mse = np.sum(np.sum((x_npy - y_npy)**2, axis=0))
 
 class LeNet5(nn.Module):
     """
-    Input - 1x32x32
-    C1 - 6@28x28 (5x5 kernel)
+    Input - 1x200x200
+    C1 - 6@196x196 (5x5 kernel)
     tanh
-    S2 - 6@14x14 (2x2 kernel, stride 2) Subsampling
-    C3 - 16@10x10 (5x5 kernel, complicated shit)
+    S2 - 6@98x98 (2x2 kernel, stride 2) Subsampling
+    C3 - 16@94x94 (5x5 kernel)
     tanh
-    S4 - 16@5x5 (2x2 kernel, stride 2) Subsampling
-    C5 - 120@1x1 (5x5 kernel)
+    S4 - 16@47x47 (2x2 kernel, stride 2) Subsampling
+    C5 - 32@43x43 (5x5 kernel)
+    tanh
+    S5 - 32@21x21 (3x3 kernel, stride 2) Subsampling
+    C6 - 64@17x17 (5x5 kernel)
+    tanh
+    S6 - 64@8x8 (3x3 kernel, stride 2) Subsampling
+    C7 - 120@1x1 (8x8 kernel)
+    tanh
+
     F6 - 84
     tanh
     F7 - 10 (Output)
@@ -43,14 +59,20 @@ class LeNet5(nn.Module):
     def __init__(self):
         super(LeNet5, self).__init__()
         self.convnet = nn.Sequential(OrderedDict([
-            ('c1', nn.Conv1d(1, 6, kernel_size=(5, 5))),
+            ('c1', nn.Conv2d(1, 6, kernel_size=5)),
             ('relu1', nn.ReLU()),
-            ('s2', nn.MaxPool1d(kernel_size=(2, 2), stride=2)),
-            ('c3', nn.Conv1d(6, 16, kernel_size=(5, 5))),
+            ('s2', nn.MaxPool2d(kernel_size=2, stride=2)),
+            ('c3', nn.Conv2d(6, 16, kernel_size=5)),
             ('relu3', nn.ReLU()),
-            ('s4', nn.MaxPool1d(kernel_size=(2, 2), stride=2)),
-            ('c5', nn.Conv1d(16, 120, kernel_size=(5, 5))),
-            ('relu5', nn.ReLU())
+            ('s4', nn.MaxPool2d(kernel_size=2, stride=2)),
+            ('c5', nn.Conv2d(16, 32, kernel_size=5)),
+            ('relu5', nn.ReLU()),
+            ('s5', nn.MaxPool2d(kernel_size=3, stride=2)),
+            ('c6', nn.Conv2d(32, 64, kernel_size=5)),
+            ('relu6', nn.ReLU()),
+            ('s6', nn.MaxPool2d(kernel_size=3, stride=2)),
+            ('c7', nn.Conv2d(64, 120, kernel_size=8)),
+            ('relu7', nn.ReLU()),
         ]))
 
         self.fc = nn.Sequential(OrderedDict([
@@ -73,7 +95,7 @@ def generate_training_data(n, train_perc=0.8):
 
     for i in range(n):
         params, img = noisy_circle(200, 50, 2)
-        param_image_list.append((params, img))
+        param_image_list.append((params, np.expand_dims(img, axis=0)))
     train_data, val_data = param_image_list[0:int(n * train_perc)], param_image_list[int(n * train_perc):]
 
     return train_data, val_data
@@ -82,10 +104,14 @@ def generate_training_data(n, train_perc=0.8):
 # Generate data
 train_data, val_data = generate_training_data(n=2000)
 
-data_train_loader = DataLoader(train_data, batch_size=256, shuffle=True, num_workers=8)
+# class CircleDataset(Dataset):
+#     def __init__(self, data):
+#         self.samples = data
+
+data_train_loader = DataLoader(train_data, batch_size=256, shuffle=False, num_workers=8)
 data_test_loader = DataLoader(val_data, batch_size=1024, num_workers=8)
 
-net = LeNet5()
+net = LeNet5().float()
 viz = visdom.Visdom()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(net.parameters(), lr=2e-3)
@@ -95,8 +121,8 @@ cur_batch_win_opts = {
     'title': 'Epoch Loss Trace',
     'xlabel': 'Batch Number',
     'ylabel': 'Loss',
-    'width': 1200,
-    'height': 600,
+    'width':  200,
+    'height': 200,
 }
 
 
@@ -104,10 +130,11 @@ def train(epoch):
     global cur_batch_win
     net.train()
     loss_list, batch_list = [], []
-    for i, (images, labels) in enumerate(data_train_loader):
+    for i, (labels, images) in enumerate(data_train_loader):
         optimizer.zero_grad()
-
-        output = net(images)
+        # import ipdb
+        # ipdb.set_trace()
+        output = net(images.float())
 
         loss = criterion(output, labels)
 
@@ -145,3 +172,12 @@ def test():
 def train_and_test(epoch):
     train(epoch)
     test()
+
+
+def main():
+    for e in range(1, 2):
+        train_and_test(e)
+
+
+if __name__ == '__main__':
+    main()
